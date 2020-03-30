167e3b27fa7b8dbb012c0f5db4f5fb83c65f5d08
Cheaanggish <61009698+Cheaanggish@users.noreply.github.com>
2020-03-30 21:10:26 +0700

    Add files via upload

---
+++ modul 6.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h> 
+#include<string.h>
+
+char balik(char x[50]);
+
+char cek(char y[50],char z[50]);
+
+int main()
+
+{
+char m[50],o[50];
+
+printf("Masukkan kalimat : "); gets(m);
+
+strcpy(o,m);
+
+balik(o);
+
+cek(m,o);
+
+getch();
+
+printf("Masukkan kalimat : "); gets(m);
+strcpy(o,m);
+
+balik(o);
+
+cek(m,o);
+
+getch();
+
+}
+
+
+char balik(char x[50])
+
+{
+
+strrev(x);
+
+}
+ 
+char cek(char y[50],char z[50])
+
+{
+
+if(strcmp(y,z)==0)
+
+{
+
+printf("\nKalimat tersebut adalah palindrom\n\n");
+
+}
+
+else
+
+{
+
+printf("\nKalimat tersebut bukan palindrom\n\n");
+
+}
+
+}
+
